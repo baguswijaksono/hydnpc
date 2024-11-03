f12ef1ede1d7f0b0ba7909670dc16a111ba339c4
Bagus Muhammad Wijaksono <106671990+baguswijaksono@users.noreply.github.com>
2024-11-03 20:38:16 +0700

    feat: auth token

--- db.php
+++ db.php
@@ -3,6 +3,7 @@ $servername = "skibidi";
 $username = "skibidi";
 $password = "skibidi";
 $dbname = "skibidi";
+$token = "your_token_here";  // Replace with your actual token
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
