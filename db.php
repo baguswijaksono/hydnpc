c9add09bb866c6cbec2651b2e50b0768d483bc53
Bagus Muhammad Wijaksono <106671990+baguswijaksono@users.noreply.github.com>
2024-11-03 20:31:07 +0700

    Create db.php

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "skibidi";
+$username = "skibidi";
+$password = "skibidi";
+$dbname = "skibidi";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
